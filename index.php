d92b603a3f038c3851b8311a2c21e9701c4e80b7
Mawerick102 <145151266+Mawerick102@users.noreply.github.com>
2023-09-15 23:34:24 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php>
-echo "<h2> Hello <?>;
-</?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Тестируем PHP</title>
+    </head>
+    <body>
+        <?php echo '<p>Привет, мир!</p>'; ?>
+    </body>
+</html>
